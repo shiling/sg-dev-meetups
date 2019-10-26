929ab83a906cbb1f3b0e85687db775c394b7852b
gphofficial <gphofficial@gmail.com>
2019-10-26 20:17:03 +0800

    [WIP] create event

--- pages/event/create.vue
+++ pages/event/create.vue
@@ -1,31 +1,130 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        CREATE EVENT PAGE
-      </h1>
-      <h2 class="subtitle">
-        Platform for SG software developers to meetup
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <!-- text-center -->
+    <div class="container" style="padding-top:10px">
+          <div>
+            <h1>Create an event</h1>
+            <div class="text-muted">{{$route.params.slug}} Meetup Group</div>
+          </div>
+          <hr>
+          
+          <div class="border rounded p-3 mb-3 text-left bg-blue">
+            <b-form id="login-form">
+    
+              <b-form-group
+                id="login-form__email-group"
+                label="Title (Required)"
+                label-for="login-form__email-input"
+              >
+                <b-form-input
+                  id="login-form__email-input"
+                  type="text"
+                  required
+                ></b-form-input>
+              </b-form-group>
+
+              <b-form-group
+                id="login-form__email-group"
+                label="Date and Time"
+                label-for="login-form__email-input"
+              >
+
+                <div class="row">
+                  <div class="col">
+                    <input type="text" class="form-control" placeholder="Date">
+                  </div>
+                  <div class="col">
+                    <input type="text" class="form-control" placeholder="Time">
+                  </div>
+                </div>
+              </b-form-group>
+
+              <b-form-group
+                id="login-form__email-group"
+                label="Duration"
+                label-for="login-form__email-input">
+                  <b-form-select v-model="selected" class="mb-3">
+                    <option value="1">1 hour</option>
+                    <option value="1.5">1.5 hour</option>
+                    <option value="2">2 hour</option>
+                    <option value="-1" v-on:click="durationSwitchEndTime()">Others</option>
+                  </b-form-select>
+                    <div class="row" style="display:none;" id="endtime">
+                        <div class="col">
+                          <input type="text" class="form-control" placeholder="Date">
+                        </div>
+                        <div class="col">
+                          <input type="text" class="form-control" placeholder="Time">
+                        </div>
+                    </div>
+              </b-form-group>
+
+              
+
+              <b-form-group
+                id="login-form__email-group"
+                label="Featured Photo"
+                label-for="login-form__email-input">
+                <b-form-file accept="image/*"></b-form-file>
+              </b-form-group>
+
+
+              <b-form-group
+                id="login-form__email-group"
+                label="Description"
+                label-for="login-form__email-input"
+              >
+
+                <b-form-textarea
+                  id="textarea"
+                  v-model="text"
+                  placeholder="Enter something..."
+                  rows="3"
+                  max-rows="6">
+                </b-form-textarea>
+
+                <pre class="mt-3 mb-0">{{ text }}</pre>
+
+              </b-form-group> 
+
+              <b-form-group
+                id="login-form__email-group"
+                label="Location"
+                label-for="login-form__email-input"
+              >
+              <b-input-group prepend="📍" class="mb-2 mr-sm-2 mb-sm-0">
+                  <b-input id="inline-form-input-username" placeholder=""></b-input>
+                </b-input-group>
+              </b-form-group> 
+
+              <p class="text-muted">Recent Places</p>
+                <div style="padding-top:10px;padding-bottom:10px;">
+                <div class="row flex-nowrap" style="overflow-x:scroll;overflow-y:hidden">
+                  <b-col md="3" v-for="item in [{location: 'asd', address: '123'},{location: 'asd', address: '123'},{location: 'asd', address: '123'},{location: 'asd', address: '123'},{location: 'asd', address: '123'},{location: 'asd', address: '123'},{location: 'asd', address: '123'},{location: 'asd', address: '123'},{location: 'asd', address: '123'},{location: 'asd', address: '123'},{location: 'asd', address: '123'},{location: 'asd', address: '123'},{location: 'asd', address: '123'}]">
+                    <div class="card">
+                      <div>
+                        <p>{{ item.location }}</p>
+                      </div>
+                      <div class="text-muted">{{ item.address }}</div>
+                      <div class="text-center" style="padding-top:5px;">
+                        <button>Select</button>
+                      </div>
+                    </div>
+                  </b-col>
+                </div>
+              </div>
+              
+
+                
+
+              
+    
+              <b-button variant="primary">Submit</b-button>
+            </b-form>
+    
+          </div>
+    
+    
       </div>
-    </div>
-  </div>
 </template>
 
 <script>
@@ -34,39 +133,52 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
+  },
+  methods: {
+    durationSwitchEndTime: function () {
+      document.getElementById("durationDropDown").style.display = "none"
+      document.getElementById("endtime").style.display = "flex"
+    }
+  },
+  data: {
+    items: [
+      { title: 'Foo' },
+      { title: 'Bar' }
+    ]
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<!-- <style>
+    .container {
+      margin: 0 auto;
+      min-height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+    }
+    
+    .title {
+      font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+        'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+      display: block;
+      font-weight: 300;
+      font-size: 100px;
+      color: #35495e;
+      letter-spacing: 1px;
+    }
+    
+    .subtitle {
+      font-weight: 300;
+      font-size: 42px;
+      color: #526488;
+      word-spacing: 5px;
+      padding-bottom: 15px;
+    }
+    
+    .links {
+      padding-top: 15px;
+    }
+    </style>
+     -->
