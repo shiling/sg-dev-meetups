8637fc9dfd0c1eb67627645c07e75374f9e620ee
auzadventure@gmail.com <auzadventure@gmail.com>
2019-10-27 22:15:58 +0800

    Group Page

--- pages/event/_id/index.vue
+++ pages/event/_id/index.vue
@@ -9,44 +9,8 @@
       </div>
      <div class="col-md-6">
     
-     <h3> This is the Event Title {{$route.params.id}}</h3>
-     <p>From <router-link :to="''">Group</router-link></p>
-     <p class='text-muted'>
-         Platform for SG software developers to meetup
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-      </p>
-
-
-     <div class="card mb-4">
-        <div class="card-body">
-          <div class="row">
-            <div class="col-sm-1">
-              <fa :icon="['far','clock']" class='fa-2x'/>
-            </div>
-            <div class="col-sm-11">
-              <h5>
-                14:00, 24 Nov 2019
-              </h5>
-              <span class='small'>in <b>2</b> days <b>18</b> hours</span>
-            </div>
-          </div>          
-          <div class="row mt-4">
-            <div class="col-sm-1">
-              <fa :icon="['fa','map-marker-alt']" class='fa-2x'/>
-            </div>
-            <div class="col-sm-11">
-              <h5>
-                20 Guilmade Road
-                <br>
-                Singapore 445555
-              </h5>
-            </div>
-          </div>
-
-        </div>
-      </div>
-        
-
+     <h1 class='mega'> This is the Event Title {{$route.params.id}}</h1>
+     <p>From <router-link :to="'/group/'">Group</router-link></p>
 
 
       <div class="mt-5" style='font-size:25px'>
@@ -76,6 +40,16 @@
 
   <div class="row">
     <div class="col-md-6">
+      <h3>Description</h3>
+      <p class='text-muted'>
+         Platform for SG software developers to meetup
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+      </p>
+
+      <div class="my-2">
+        &nbsp;
+      </div>
+
       <h3>Attending (10)</h3>
 
       <div class="row rsvp mt-4">
@@ -103,16 +77,45 @@
       </div>
     </div>
     <div class="col-md-6">
-        <h3> Posts (20)</h3>
-        <div class="mt-4">
-          <div class="card">
-            <div class="card-body">
-              <div class="">How about this as a post?</div>
-              <p class='small'>14:23  23 Nov 2019 By Steven</p>
-               <hr> 
+     <div class="card mb-4">
+        <div class="card-body">
+          <div class="row">
+            <div class="col-sm-1">
+              <fa :icon="['far','clock']" class='fa-2x'/>
+            </div>
+            <div class="col-sm-11">
+              <h5>
+                14:00, 24 Nov 2019
+              </h5>
+              <span class='small'>in <b>2</b> days <b>18</b> hours</span>
+            </div>
+          </div>          
+          <div class="row mt-4">
+            <div class="col-sm-1">
+              <fa :icon="['fa','map-marker-alt']" class='fa-2x'/>
+            </div>
+            <div class="col-sm-11">
+              <h5>
+                20 Guilmade Road
+                <br>
+                Singapore 445555
+              </h5>
             </div>
           </div>
         </div>
+      </div>
+
+
+      <h3> Posts (20)</h3>
+      <div class="mt-4">
+        <div class="card">
+          <div class="card-body">
+            <div class="">How about this as a post?</div>
+            <p class='small'>14:23  23 Nov 2019 By Steven</p>
+             <hr> 
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 
