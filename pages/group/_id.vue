1e17b114563fedea467630f7812aa771935d58be
auzadventure@gmail.com <auzadventure@gmail.com>
2019-10-27 08:15:48 +0800

    updated the group page

--- pages/group/_id.vue
+++ pages/group/_id.vue
@@ -14,49 +14,66 @@
           <h5 class='mt-4'>
             Platform for SG software developers to meetup
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-          </h5> 
-          <div class="mt-4">
-            <i class="fa fa-clock"></i>dd
-          </div>     
-          <div class="">
-            <i class="fa fa-address-book" aria-hidden="true"></i>
-          </div>     
-          <div class="">
-            <i class="fa fa-address-book" aria-hidden="true"></i>
-          </div>     
+          </h5>
+
+          <div class="details mt-4 ml-2">
+            <div>
+              <fa :icon="['fas', 'map-marker-alt']"/> Singapore, Singapore
+            </div>
+            <div class="">
+              <fa :icon="['fa','users']"/> 1000 members - Public Group 
+            </div>     
+            <div class="">
+              <fa :icon="['fa','user-cog']"/> Organizers Lionel chardy
+            </div>  
+
+          </div>
+
+          
+             
       </div>
     </div>
 
-    <div class="py-3">
-      <hr>  
+    <hr class='my-4'>
+    <div class="row my-2">
+      <div class="col-md-6">
+        <div class="list-group list-group-horizontal" id="myList" role="tablist">
+          <a class="list-group-item list-group-item-action" data-toggle="list" href="#home" role="tab">Events</a>
+          <a class="list-group-item list-group-item-action" data-toggle="list" href="#profile" role="tab">Chats</a>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <button class='btn btn-primary'> Join </button>
+        
+      </div>
     </div>
     
     <div class="row mt-4">
       
         <div class="col-md-6">
               
-            <h2>Events</h2>
+            <h2>Events (10)</h2>
 
-            <div class="card">
+            <div class="card mt-2">
               <div class="card-body">
                  <h6>12 Nov 19</h6>
                  <h4>Vue.js Meeting</h4>
-                 <div class="">
-                       <i class='fa'> HackerSpace</i>
-                 </div>
-                 <div class="row">
-                     <div class="col-md-6">
-                       48 Attendees
-                     </div>
-                     <div class="col-md-6">
-                       <button class='btn btn-primary'>
-                         Join
-                       </button>
-                     </div>
-                 </div>
                  <div class="text-muted">
                       Lorem Ipsum 300
                  </div>
+                 <div class="row">
+                     <div class="col-md-6">
+                      
+                        <fa :icon="['fa','users']" /> 48 Attendees
+                        
+
+                     </div>
+                     <div class="col-md-6">
+                        <fa :icon="['fa','map-marker-alt']" /> 99 HackerSpace, Lavender
+                                               
+                     </div>
+                 </div>
+                 
               </div>
             </div>
 
@@ -113,4 +130,12 @@ export default {
   .profile {
     height: 60px;
   }
+  .details div {
+    margin-bottom: 5px
+  }
+  .details svg {
+    display:inline-block;
+    width: 30px;
+  }
+
 </style>
